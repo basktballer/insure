db10e75989907f3db7bc4ad49187a1e82186fa19
Eden Yeung <basktballer@gmail.com>
2020-03-22 18:31:40 -0400

    responsive design complete

--- js/insure.js
+++ js/insure.js
@@ -1,5 +1,15 @@
 const hamburgerMenu = document.getElementById("navbar-hamburger");
+const closeButton = document.getElementById("navbar-close");
+const navMenu = document.getElementById("nav-mobile");
 
 hamburgerMenu.addEventListener('click', function (event) {
-  alert('Element clicked through function!');
+  navMenu.style.display = "flex";
+  closeButton.style.display = "flex"
+  hamburgerMenu.style.display = "none";
+});
+
+closeButton.addEventListener('click', function (event) {
+  navMenu.style.display = "none";
+  closeButton.style.display = "none"
+  hamburgerMenu.style.display = "flex";
 });
